a6528a7106192978d6536bb943320b487f658948
hien040599 <60846482+hien040599@users.noreply.github.com>
2020-03-29 22:05:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!--<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="check.php" method="POST">
+            <h1>LOGIN</h1>
+            <input type="text" placeholder="username" required="" name="txtuser">
+            <input type="password" placeholder="password" required="" name="txtpass">
+            <button type="submit" name="btnsubmit">submit</button>
+        </form>
+    </body>
+</html>-->
+<!--<html>
+    <head>
+        <title>Đăng kí</title>
+        <link rel="stylesheet" href="CSS/1.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,400i|Noto+Sans:400,400i,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
+        <link href="nbproject/css.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    </head>
+   
+    <body>
+        <div class="to">
+            <div class="form">
+                 
+                <h2>LOGIN</h2>
+                
+             <i class="fa fa-user fa-4x" aria-hidden="true"></i>
+                <form action="check.php" method="POST">
+                <label style="margin-left: 1px;">Username</label>
+                <input type="text" name="txtuser">
+                <label style="margin-left: 1px;">Password</label>
+                <input type="password" name="txtpass">    
+                <button  type="submit" name="btnsubmit">Login</button>
+                </form>
+         
+            </div> 
+        </div>
+    </body>
+</html>-->
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login Form Design</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link href="nbproject/css.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="CSS/1.css">
+       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+     
+    <script src="main.js"></script>
+</head>
+<body>
+    <section>
+        <div class="container">
+            <div class="form_content">
+<!--                <i class="fa fa-user fa-5x" aria-hidden="true"></i>-->
+                <img class="img10" src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-business-male-icon-vector-png-image_4187852.jpg" alt="image"/>
+            </div>
+            <div class="login_form">
+                <h1>Sign In</h1>
+                <form action="index1.php"  method="POST" >
+                    <input type="text" name="txtuser" required="" placeholder="Username">
+                    <input type="password" name="txtpass" required="" placeholder="Password">
+                    <input type="submit" name="btnsubmit" value="Login">
+                </form>
+                <a href="#">Forget Password</a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
