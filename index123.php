95c31163f140ccd4fce80e138f8020e8f374898b
hien040599 <60846482+hien040599@users.noreply.github.com>
2020-04-01 11:39:38 +0700

    Add files via upload

---
+++ index123.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       $conn="host=ec2-52-87-58-157.compute-1.amazonaws.com post=5432 user=vbvfmzkrwcdhub password=8ba1bca82f6b4d577ab221c7b6b3705bccf4ed5e1085307c14c13a7b81c3a5b6";
+       $cnn= pg_connect($conn);
+        ?>
+    </body>
+</html>
