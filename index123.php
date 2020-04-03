813bdac4c3b6455b83f7bbbd0b5b862a887d5283
hien040599 <60846482+hien040599@users.noreply.github.com>
2020-04-03 09:56:56 +0700

    Update index123.php

--- index123.php
+++ index123.php
@@ -8,18 +8,18 @@ and open the template in the editor.
         <?php
        $conn="host=ec2-52-87-58-157.compute-1.amazonaws.com post=5432 user=vbvfmzkrwcdhub password=8ba1bca82f6b4d577ab221c7b6b3705bccf4ed5e1085307c14c13a7b81c3a5b6";
        $cnn= pg_connect($conn);
-       if(isset($_POST['_use'])){
-          $usename=$_POST['_use'];
+       if(isset($_POST['txtuser'])){
+          $usename=$_POST['txtuser'];
        }
-       if(isset($_POST['_pass'])){
-          $pass=$_POST['_pass'];
+       if(isset($_POST['txtpass'])){
+          $pass=$_POST['txtpass'];
        }
        $result= pg_query($cnn,"select * from login where _use = '" . $username . "' and _pass = '" . $pass."'");
 
-        while($row= pg_fetch_row($result)){
-            if($row[0]=$usename && $row[1]=$pass){
-                echo 'success';
-            }
+        if($result){
+                echo "thanh cong";
+        } else {
+                echo 'that bai';
         } 
         ?>
   
