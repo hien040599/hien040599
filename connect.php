0082813e426a2764abc01bd9f7584ff8902c9ae1
hien040599 <60846482+hien040599@users.noreply.github.com>
2020-04-04 15:44:05 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 
 <?php
-$conn = "host=ec2-52-87-58-157.compute-1.amazonaws.com post=5432 user=vbvfmzkrwcdhub password=8ba1bca82f6b4d577ab221c7b6b3705bccf4ed5e1085307c14c13a7b81c3a5b6";
+$conn = "host=ec2-52-87-58-157.compute-1.amazonaws.com port=5432 user=vbvfmzkrwcdhub password=8ba1bca82f6b4d577ab221c7b6b3705bccf4ed5e1085307c14c13a7b81c3a5b6";
 $cnn = pg_connect($conn);
 if (isset($_POST['_use'])) {
     $usename = $_POST['_use'];
